f70aa11d505a6785ffa54b3d167395b896fb6760
knockoutned <knockoutned@users.noreply.github.com>
2015-09-27 09:36:51 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a matrix and stores the inverse for future computations.
+## This saves time and processing power.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of four functions
+## set the value of a matrix
+## get returns the matrix
+## setinverse computes the inverse
+## getinverse returns the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  ##This creates a blank matrix x
+   inv <- NULL  ##Sets the default value for m within the function equal to NULL 
+   set <- function(y) {
+      x <<- y  ##Sets the value of y outside of this function equal to x
+      inv <<- NULL ##Sets the value of inv outside of this function equal to NULL
+   }
+   get <- function() x ##Assigns value for function 'get' equal to the input matrix
+   setinverse <- function(solve) inv <<- solve ##Assigns values for 'setinverse', and 'inv' in the outside environment
+   getinverse <- function(solve) inv ##Returns inv (which is NULL in this case)
+   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of the matrix, but tests if the inverse has already been calculated.
+## If it has, it moves on. If it has not, it calculates it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   inv <- x$getinverse() ##Sets inv equal to value of getinverse function (which is NULL)
+   if(!is.null(inv)) {  ##Conditional: if not equal to NULL (has the inverse already been calculated)
+      message ("getting cached data.")
+      return(inv)
+   }
+   data <- x$get() ##Sets data equal to the results of get() function
+   inv <- solve(data) ##Calculates the inverse of data (which equals x) and stores it as inv
+   x$setinverse(inv)  ##Stores inverse value in the cache using the setinverse function
+   inv
 }
+
